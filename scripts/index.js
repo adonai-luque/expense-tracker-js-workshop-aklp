7a7b21cd96b3052f16ca9088e9f97ec53eef2902
adonai-luque <akluqueprieto@gmail.com>
2021-12-28 14:29:46 -0500

    ExpensesPage module created

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,37 @@
-let expenses = getExpenses();
-renderExpenses(expenses);
+// let expenses = getExpenses();
+// renderExpenses(expenses);
+
+const ExpensesPage = (() => {
+  const expenses = [1, 2, 3];
+  const toString = () => {
+    return `
+      <h1>Expenses Page</h1>
+      <ul>
+        ${expenses.map((e) => `<li>${e}</li>`).join("")}
+      </ul>
+    `;
+  }
+  const addExpense = (expense) => expenses.push(expense)
+  return {
+    toString,
+    addExpense,
+  };
+})();
+
+const newPage = (() => {
+  const toString = () => {
+    return `
+      <h1>New Page</h1>
+
+      
+    `
+  }
+  return {
+    toString
+  }
+})();
+
+const root = document.querySelector("#root");
+
+const load = (module) => root.innerHTML = module;
+load(newPage)
